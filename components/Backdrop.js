7e765f5282ad6b8fbedb79b35b59d30a595b37e6
bhargavjagtap <bhargav98jagtap@gmail.com>
2022-02-11 14:53:31 +0530

    Backdrop file

--- components/Backdrop.js
+++ components/Backdrop.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
-function Backdrop({children, onClick }) {
+const Backdrop = ({ children, onClick }) => {
   return (
     <motion.div
-        onClick={onClick}
-        className="absolute top-0 left-0 h-full w-full overflow-y-scroll bg-black/70 flex itmes-center justify-center z-50"  
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
+      onClick={onClick}
+      className="absolute top-0 left-0 h-full w-full overflow-y-scroll bg-black/70 flex items-center justify-center z-50"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
-        {children}
+      {children}
     </motion.div>
-  )
-}
+  );
+};
 
 export default Backdrop;
